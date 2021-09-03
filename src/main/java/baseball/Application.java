1301fa2a0f4c73ad9f9459200655727fdd755bb7
jinbkim <topmedia3@naver.com>
2021-09-03 12:52:19 +0900

    ADD jinbkim review

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,8 +13,8 @@ public class Application {
 			}while((!BBG.compareNum()));
 			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 			System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-			int restartNum = scanner.nextInt();
-			if(restartNum==2) {
+			int restartNum = scanner.nextInt();   // 입력값에 대한 예외처리 하기
+			if(restartNum==2) {  // 상수 하드코딩 하지 말기
 				break;
 			}
 			else {
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -9,7 +9,7 @@ public class BaseballGame {
 	private int[] gameRandomNum = new int [3];
 	private int[] userInputNum = new int [3];
 	public BaseballGame() {
-		int Rnum=0;
+		int Rnum=0;  // 그냥 randomNum 이렇게 축약하지 않으면 더 좋을듯?
 		while(gameRandomNum[0]==gameRandomNum[1]||gameRandomNum[1]==gameRandomNum[2]||gameRandomNum[0]==gameRandomNum[2]) {
 			for(int i = 0;i<gameRandomNum.length;i++) {
 				Rnum = RandomUtils.nextInt(1, 9);
@@ -24,13 +24,14 @@ public class BaseballGame {
 	public void userInput(Scanner sc) {
 		String input;
 		System.out.print("숫자를 입력해주세요 : ");
-		input = sc.next();
-		if(input.length()==3) {
+		input = sc.next();   // 입력값에 대한 예외처리 부족
+		if(input.length()==3) {   // 상수 하드코딩 하지 말기
 			for(int i = 0;i<userInputNum.length;i++) {
 				userInputNum[i] = Integer.parseInt(String.valueOf(input.charAt(i)));
 			}
 		}
 		else {
+			// try ~ catch 문을 사용해야 throw 했을때 예외처리를 할수 있음
 			throw new IllegalArgumentException("잘못된 입력입니다. 다시 입력하세요.");
 		}
 	}
